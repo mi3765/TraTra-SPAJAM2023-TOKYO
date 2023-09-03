475e9ec76af0822f4f9fdf037c5f8ca764e4c463
mi3765 <chips1225@outlook.jp>
2023-09-03 12:24:14 +0900

    change img sensoji

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,12 @@
 export const Card = () => {
-    return (
-        <div>
-            <div className="flex flex-col">
-                <img src="./icon.png" alt="icon" />
-                <h3>RouteName</h3>
-                <p>Tag</p>
-                <p>Price</p>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<div className="flex flex-col">
+				<img src="./sensoji.jpg" alt="icon" />
+				<h3>浅草寺</h3>
+				<p>Tag</p>
+				<p>¥500</p>
+			</div>
+		</div>
+	);
 };
