ee5a89af1324a6705ac14650486bb2b095a8803a
mi3765 <chips1225@outlook.jp>
2023-09-03 11:26:52 +0900

    install firebase

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header"
-import { Detail } from "@/pages/detail/Detail.jsx"
 import { Footer } from "@/components/Footer ";
 // import { RouteCreationEdit } from "@/pages/map/RouteCreationEdit";
 import { CardList } from "@/pages/CardList";
@@ -9,7 +8,6 @@ export default function Home() {
     <>
       <Header />
       <CardList />
-      <Detail />
       {/* <RouteCreationEdit/> */}
       <Footer />
     </>
